9eafef77dccc5e194d99590cab90a64688128719
jona <jona.wehnes@gmail.com>
2024-03-01 10:20:43 +0100

    variabelen

---
+++ m3prog_project/public/01/variabelen.php
@@ -0,0 +1,23 @@
+<?php
+
+$naam = "Ik ben jona";
+$woon = "Ik woon in diemen";
+$leeftijd = "ik ben 16 jaar oud";
+$temp = 7.5;
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php echo "$naam $woon $leeftijd het is $temp graden"  ?>
+
+</body>
+</html>
